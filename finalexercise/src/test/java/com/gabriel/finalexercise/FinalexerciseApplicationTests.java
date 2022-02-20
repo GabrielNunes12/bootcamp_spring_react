7932566fa20b84eb7de7a15cacff804e454fda60
Gabriel <prismaticnetwork12@gmail.com>
2022-02-19 22:47:29 -0300

    feat: started project

---
+++ finalexercise/src/test/java/com/gabriel/finalexercise/FinalexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.finalexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
